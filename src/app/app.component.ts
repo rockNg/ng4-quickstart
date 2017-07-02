f54d183f29770dbafa3c11d2e2544ba2e8e6c4e4
rockGithub <rock20110414@gmail.com>
2017-07-02 11:54:36 +0800

    fucking changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<div>
+			  <label>name: </label>
+			  <input [(ngModel)]="hero.name" placeholder="name">
+			</div>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+	name = 'Angular';
+	hero: Hero = {
+	  id: 1,
+	  name: 'Windstorm'
+	}
+	
+}
